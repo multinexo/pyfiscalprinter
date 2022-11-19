bed57a6905878b5d1a501a59d0ab7255345fb94b
Matias Ahumada <ahumadamatias7@gmail.com>
2022-11-19 13:16:39 -0300

    added/audit-when-close-daily auto-commit

--- pyfiscalprinter/epson_ext_driver.py
+++ pyfiscalprinter/epson_ext_driver.py
@@ -8,6 +8,7 @@ class EpsonFiscalExtDriver(EpsonFiscalDriver):
 
     def __init__(self, deviceFile, speed=9600):
         connect = True
+	print('(EpsonExt) Entre por lo tanto me toma lo que estoy haciendo')
         while connect:
             try:
                 os.system("./fiscalproxy --comm-port=1 -p 3000 -l &")
@@ -19,11 +20,11 @@ class EpsonFiscalExtDriver(EpsonFiscalDriver):
 
     def _parseReply( self, reply, skipStatusErrors ):
         fields = reply.split('|')
-        printerStatus = fields[2]
-        fiscalStatus = fields[1]
-        if not skipStatusErrors:
-            self._parsePrinterStatus( printerStatus )
-            self._parseFiscalStatus( fiscalStatus )
+        printerStatus = fields[1]
+        fiscalStatus = fields[2]
+        #if not skipStatusErrors:
+            #self._parsePrinterStatus( printerStatus )
+            #self._parseFiscalStatus( fiscalStatus )
         return fields[1:]
 
     def sendCommand( self, commandNumber, fields, skipStatusErrors = False ):
@@ -33,7 +34,7 @@ class EpsonFiscalExtDriver(EpsonFiscalDriver):
 
         url = 'http://localhost:3000'
         headers = {'Content-Type': 'text/plain'}
-
+	print('Comando: ', message)
         ret = ''
         while ret == '':
             try:
@@ -44,6 +45,6 @@ class EpsonFiscalExtDriver(EpsonFiscalDriver):
                 time.sleep(5)
                 print("Was a nice sleep, now let me continue...")
                 continue
-
+	print('Respuesta: ', ret.content)
         return self._parseReply( ret.content, skipStatusErrors )
 
